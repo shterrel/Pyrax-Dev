a32676186c6c56b41212f073ba1edb9dd4918b50
shterrel <shawn.terrell@rackspace.com>
2014-08-28 14:52:18 +0000

    Production scripts

--- Production_ready/DR-Image-ExImp.py
+++ Production_ready/DR-Image-ExImp.py
@@ -53,10 +53,6 @@ while img.status != "ACTIVE":
 
 print("Image created.")
 
-#pyrax.set_default_region("DFW")
-#pyrax.set_setting("identity_type", "rackspace")
-#pyrax.set_credential_file("/root/.rackspace_cloud_credentials")
-
 cf_dfw = pyrax.connect_to_cloudfiles("DFW")
 
 cont = cf_dfw.create_container("Export")
@@ -119,3 +115,69 @@ answer = raw_input("Do you want to track the task until completion? This may "
 if answer and answer[0].lower() == "y":
     pyrax.utils.wait_until(task, "status", ["success", "failure"],
             verbose=True, interval=30)
+
+TURBOLIFT="turbolift -u davi5652 -a d0cf0ae5007b421ba7371d9a7c469953 --os-auth-url dfw clone -sc Export -tc Import -tr iad"
+print("You will need an image file stored in a Cloud Files container.")
+conts = cf_iad.list()
+print()
+print("Select the container containing the image to import:")
+for pos, cont in enumerate(conts):
+    print("[%s] %s" % (pos, cont.name))
+snum = raw_input("Enter the number of the container: ")
+if not snum:
+    exit()
+try:
+    num = int(snum)
+except ValueError:
+    print("'%s' is not a valid number." % snum)
+    exit()
+if not 0 <= num < len(conts):
+    print("'%s' is not a valid container number." % snum)
+    exit()
+cont = conts[num]
+
+print()
+print("Select the image object:")
+objs = cont.get_objects()
+for pos, obj in enumerate(objs):
+    print("[%s] %s" % (pos, obj.name))
+snum = raw_input("Enter the number of the image object: ")
+if not snum:
+    exit()
+try:
+    num = int(snum)
+except ValueError:
+    print("'%s' is not a valid number." % snum)
+    exit()
+if not 0 <= num < len(objs):
+    print("'%s' is not a valid object number." % snum)
+    exit()
+obj = objs[num]
+
+fmt = raw_input("Enter the format of the image [VHD]: ")
+fmt = fmt or "VHD"
+base_name = os.path.splitext(os.path.basename(obj.name))[0]
+obj_name = raw_input("Enter a name for the imported image ['%s']: " % base_name)
+obj_name = obj_name or base_name
+
+task = imgs.import_task(obj, cont, img_format=fmt, img_name=obj_name)
+print("Task ID=%s" % task.id)
+print()
+answer = raw_input("Do you want to track the task until completion? This may "
+        "take several minutes. [y/N]: ")
+if answer and answer[0].lower() == "y":
+    pyrax.utils.wait_until(task, "status", ["success", "failure"],
+            verbose=True, interval=30)
+    print()
+    if task.status == "success":
+        print("Success!")
+        print("Your new image:")
+        new_img = imgs.find(name=obj_name)
+        print(" ID: %s" % new_img.id)
+        print(" Name: %s" % new_img.name)
+        print(" Status: %s" % new_img.status)
+        print(" Size: %s" % new_img.size)
+        print(" Tags: %s" % new_img.tags)
+    else:
+        print("Image import failed!")
+        print("Reason: %s" % task.message)
---
+++ Production_ready/turbolift.py
@@ -0,0 +1,2 @@
+TURBOLIFT="turbolift -u davi5652 -a d0cf0ae5007b421ba7371d9a7c469953 --os-auth-url dfw clone -sc Export -tc Import -tr iad"
+print (TURBOLIFT)
