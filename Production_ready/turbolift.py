957a00246958fc0ab40d6f5d52cbe93e4234cb3d
shterrel <shawn.terrell@rackspace.com>
2014-12-11 22:06:08 +0000

    adding

--- Production_ready/turbolift.py
+++ Production_ready/turbolift.py
@@ -1,2 +1,2 @@
-TURBOLIFT="turbolift -u davi5652 -a d0cf0ae5007b421ba7371d9a7c469953 --os-auth-url dfw clone -sc Export -tc Import -tr iad"
+TURBOLIFT="turbolift -u Squirrel -a a4e662360d0f4a65b86f81e7338f8893 --os-rax-auth dfw clone -sc Squirrel -tc Squirrel-test -tr iad"
 print (TURBOLIFT)
